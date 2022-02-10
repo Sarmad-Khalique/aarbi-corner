b716caa429fb29247ba2477cd1102c280bb74bed
Sarmad-Khalique <sarmadkhalique001@gmail.com>
2022-02-10 23:04:27 +0500

    Added User Creation function in firebase

--- src/App.js
+++ src/App.js
@@ -5,37 +5,49 @@ import Shop from "./pages/shop/Shop";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import SignInandSignUpPage from "./pages/SignInandSignUpPage/SignInandSignUpPage";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      currentUser:null,
-    }
-
+      currentUser: null,
+    };
   }
 
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
-  componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(
-      (user)=>{
-        this.setState({currentUser:user})
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      const userRef = await createUserProfileDocument(userAuth);
 
-        console.log(user)
+      //setting state if the user is logged in
+      if (userAuth) {
+        //onSnapshot checks if the snapshot has updated, using it here to get the copy of the snapshot as the snapshot will never change because we are not updating it
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          });
+        });
+      } else {
+        this.setState({
+          currentUser: userAuth,
+        });
       }
-    )
+    });
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-  
+
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Routes>
           <Route exact path="/" element={<Homepage />} />
           <Route path="/shop" element={<Shop />} />
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,6 +17,38 @@ firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+//Firestore For users
+export const createUserProfileDocument = async (userAuth, additionalUserData)=>{
+  // returning if the user is nt logged in
+  if(!userAuth) return;
+
+  
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+  
+  const userSnapShot = await userRef.get();
+  
+  if(!userSnapShot.exists){
+    const {displayName, email} = userAuth;
+    const createdAt = new Date().toDateString();
+    
+    try {
+      await userRef.set(
+        {
+          displayName,
+          email,
+          createdAt,
+          ...additionalUserData
+        }
+      )
+    } catch (error) {
+      console.log("An error occoured: ", error);
+    }
+
+  }
+
+  return userRef;
+}
+
 // Google Auth Provider with Popup
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({'prompt':'select_account'})
