6eaf207f7c7cdfe285d5021ec4ab7571b5fb2bdd
Muhammad Sarmad Khalique <sarmadkhalique001@gmail.com>
2022-02-20 18:07:31 +0500

    Added Stripe Paymet Integration

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,47 +1,48 @@
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CheckoutItem from "../../components/checkout-item/CheckoutItem";
-import { selectCartItems, selectCartItemsTotal } from "../../redux/cart/cart.selectors";
+import StripeButtonCheckout from "../../components/stripe-button-checkout/StripeButtonCheckout";
+import {
+  selectCartItems,
+  selectCartItemsTotal,
+} from "../../redux/cart/cart.selectors";
 
-const Checkout = ({cartItems, total}) => {
+const Checkout = ({ cartItems, total }) => {
   return (
     <div className="checkout-page px-6 md:px-[23%] relative">
-        <div className="checkout-table">
-            <div className="checkout-header py-2 flex justify-center border-y border-slate-400">
-                <div className="header-block w-1/2 px-2">
-                    <span>
-                        Description
-                    </span>
-                </div>
-                <div className="header-block w-1/4 text-center">
-                    <span>
-                        Quantity
-                    </span>
-                </div>
-                <div className="header-block w-1/4 text-center">
-                    <span>
-                        Price
-                    </span>
-                </div>
-            </div>
-            {
-                cartItems.length?
-                cartItems.map(
-                    cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
-                ):
-                <div className="border-b border-slate-400 p-4 text-center">Your Cart is Empty</div>
-            }
+      <div className="checkout-table">
+        <div className="checkout-header py-2 flex justify-center border-y border-slate-400">
+          <div className="header-block w-1/2 px-2">
+            <span>Description</span>
+          </div>
+          <div className="header-block w-1/4 text-center">
+            <span>Quantity</span>
+          </div>
+          <div className="header-block w-1/4 text-center">
+            <span>Price</span>
+          </div>
         </div>
-        <div className="text-3xl text-right py-2">
-            Total: $ {total}
-        </div>
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <div className="border-b border-slate-400 p-4 text-center">
+            Your Cart is Empty
+          </div>
+        )}
+      </div>
+      <div className="text-3xl text-right py-2">Total: $ {total}</div>
+      <div className="stripe-payment text-right my-4">
+        <StripeButtonCheckout price={total} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
-    cartItems: selectCartItems,
-    total: selectCartItemsTotal
-})
+  cartItems: selectCartItems,
+  total: selectCartItemsTotal,
+});
 
 export default connect(mapStateToProps)(Checkout);
